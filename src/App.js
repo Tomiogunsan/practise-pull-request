3aa7f29bdb8d85bb319d9e098c37efe212c9b9ea
Tomiogunsan <tomiafolayan@gmail.com>
2022-11-22 10:56:36 +0100

    still testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import New from './component/New';
 
 function App() {
   return (
     <div className="App">
-     welcome
+     <New />
     </div>
   );
 }
---
+++ src/component/New.jsx
@@ -0,0 +1,7 @@
+const New = () =>{
+    return <div>
+        glad to meet you
+    </div>
+}
+
+export default New
